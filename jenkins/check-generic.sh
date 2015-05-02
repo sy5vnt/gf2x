b1206e0d3dacf35f3a004ef9cbc1cffe9606b5a6
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2015-05-02 23:05:48 +0200

    more scripts

---
+++ jenkins/check-generic.sh
@@ -0,0 +1,7 @@
+export LANG=C
+export AUTOCONF_VERSION=2.69
+export AUTOMAKE_VERSION=1.13
+autoreconf -i
+./configure
+make
+make check
--- jenkins/check-openbsd-53-amd64.sh
+++ jenkins/check-openbsd-53-amd64.sh
@@ -1,7 +1,1 @@
-export LANG=C
-export AUTOCONF_VERSION=2.69
-export AUTOMAKE_VERSION=1.13
-autoreconf -i
-./configure
-make
-make check
+check-generic.sh
