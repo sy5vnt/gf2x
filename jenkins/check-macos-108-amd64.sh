2e12a2d9d2ba694f3fed3b2ad4c4bb8ae6308ea1
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2015-05-02 23:23:21 +0200

    modify script

--- jenkins/check-macos-108-amd64.sh
+++ jenkins/check-macos-108-amd64.sh
@@ -1,1 +1,4 @@
-check-generic.sh
+export PATH=$HOME/Packages/autoconf-2.69/bin:$PATH
+export PATH=$HOME/Packages/automake-1.15/bin:$PATH
+export PATH=$HOME/Packages/libtool-2.4.6/bin:$PATH
+"`dirname $0`"/check-generic.sh
