0a89e324806bc3e0b167692e7dddbd0ec1ada01e
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2015-05-03 10:44:50 +0200

    adapt to various versions of aclocal

--- jenkins/check-generic.sh
+++ jenkins/check-generic.sh
@@ -1,7 +1,18 @@
 unset `locale | cut -d= -f1`
 export LANG=C
 export AUTOCONF_VERSION=2.69
-export AUTOMAKE_VERSION=1.13
+if type aclocal-1.15 > /dev/null ; then
+    export AUTOMAKE_VERSION=1.15
+elif type aclocal-1.14 > /dev/null ; then
+    export AUTOMAKE_VERSION=1.14
+elif type aclocal-1.13 > /dev/null ; then
+    export AUTOMAKE_VERSION=1.13
+elif type aclocal > /dev/null ; then
+    :
+else
+    echo "automake (aclocal) not found" >&2
+    exit 1
+fi
 autoreconf -i
 ./configure
 make
