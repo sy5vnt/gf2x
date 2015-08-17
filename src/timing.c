sy5vnt/gf2x

commit 6966f6f9ab36f58ed9b4605aae59f7849998e575

new config check

src/timing.c
@@ -26,12 +26,15 @@
 */
 
 #include <sys/types.h>    /* for cputime */
+#ifdef HAVE_SYS_RESOURCE_H_
 #include <sys/resource.h> /* for cputime */
+#endif  /* HAVE_SYS_RESOURCE_H_ */
 #include <stdint.h>
 #include "timing.h"
 
 uint64_t microseconds()
 {
+#ifdef HAVE_SYS_RESOURCE_H_
         struct rusage res[1];
         getrusage(RUSAGE_SELF,res);
         uint64_t r;
@@ -39,4 +42,7 @@ uint64_t microseconds()
         r *= (uint64_t) 1000000UL;
         r += (uint64_t) res->ru_utime.tv_usec;
         return r;
+#else
+        return 0;
+#endif
 }

configure.ac
@@ -33,6 +33,8 @@ AC_CANONICAL_TARGET
 
 AC_LANG([C])
 
+AC_CHECK_HEADERS([sys/resource.h])
+
 AC_SUBST([gf2x_lib_version], [1:2:0])
 
 AM_INIT_AUTOMAKE([1.13])

src/tuneup_pre.c
@@ -36,7 +36,6 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <sys/types.h>
-#include <sys/resource.h>
 #include <string.h>
 
 /* gf2x is used for checking correctness -- not otherwise */

tests/bench.c
@@ -30,7 +30,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/types.h>
-#include <sys/resource.h>
 #include <math.h>		// for random
 
 #include "gf2x.h"

tests/check-and-bench-common.c
@@ -25,15 +25,18 @@
    02110-1301, USA.
 */
 
+#include "gf2x/gf2x-config.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/types.h>
+#ifdef  HAVE_SYS_RESOURCE_H_
 #include <sys/resource.h>
+#endif  /*  HAVE_SYS_RESOURCE_H_ */
 #include <gmp.h>		// for random
 
-#include "gf2x/gf2x-config.h"
 #include "gf2x/gf2x-impl.h"
 #include "gf2x/gf2x-thresholds.h"
 
@@ -45,9 +48,13 @@ static long init_extra_arg = ENGINE_EXTRA_ARG_DEFAULT;
 // cputime in millisec.
 static int cputime()
 {
+#ifdef  HAVE_SYS_RESOURCE_H_
     struct rusage rus;
     getrusage(0, &rus);
     return rus.ru_utime.tv_sec * 1000 + rus.ru_utime.tv_usec / 1000;
+#else
+    return 0;
+#endif
 }
 
 int time_total;

tests/check.c
@@ -30,7 +30,6 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/types.h>
-#include <sys/resource.h>
 #include <stdint.h>
 #include <inttypes.h>
 

commit 86c9eccdaa87a77b29afab2ce146a4376f96b382

Ooops. 6966f6f broke tuning, ci has been running for 2 months.

src/timing.c
@@ -25,6 +25,8 @@
    02110-1301, USA.
 */
 
+#include "gf2x/gf2x-config.h"
+
 #include <sys/types.h>    /* for cputime */
 #ifdef HAVE_SYS_RESOURCE_H_
 #include <sys/resource.h> /* for cputime */