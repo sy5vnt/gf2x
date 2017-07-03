7922b9721dc3c8c127d95e00b36079a2abbfb892
Emmanuel Thomé <Emmanuel.Thome@inria.fr>
2017-07-04 01:07:25 +0200

    another test

--- jenkins/check-generic.sh
+++ jenkins/check-generic.sh
@@ -6,3 +6,13 @@ make check
 if [ -f "`dirname $0`"/extra-"`basename $0`" ] ; then
     . "`dirname $0`"/extra-"`basename $0`"
 fi
+
+# do that on all architectures.
+make distclean
+./configure $configure_extra --disable-hardware-specific-code
+make
+make check
+if [ -f "`dirname $0`"/extra-"`basename $0`" ] ; then
+    . "`dirname $0`"/extra-"`basename $0`"
+fi
+
